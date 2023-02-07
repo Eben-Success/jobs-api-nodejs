d9221765c364da4df0961906c901bf8de5a715be
Eben-Success <aebenezer237@gmail.com>
2023-02-07 21:09:44 +0000

    update

--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 const {login, register} = require('../controllers/auth')
 
+
 router.post('/register', register)
 router.post('/login', login)
+
+
+module.exports = router
