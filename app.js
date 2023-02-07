c1a9ef1722a5b0ed7f5a7f9a74fdcf8a07a98042
Eben-Success <aebenezer237@gmail.com>
2023-02-07 20:54:59 +0000

    update

---
+++ app.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+require('express');
+const express = require('express');
+const app = express();
+
+
+// error handlers
+notFoundMiddleware = require('./middleware/not-Found');
+errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+// routes
+app.use('/', (req, res) => {
+    res.send('jobs-api');
+})
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        console.log(`Server is listening on port ${port}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
